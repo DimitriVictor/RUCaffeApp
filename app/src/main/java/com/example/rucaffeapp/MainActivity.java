c9ee3a2e6a95df21685435f31fcb16706837ed21
dimitrivictor1217@gmail.com <dimitrivictor1217@gmail.com>
2021-04-16 12:00:22 -0400

    Added main screeen image buttons

--- app/src/main/java/com/example/rucaffeapp/MainActivity.java
+++ app/src/main/java/com/example/rucaffeapp/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.rucaffeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton orderDonutButton;
+    private ImageButton orderCoffeeButton;
+    private ImageButton viewOrderButton;
+    private ImageButton storeOrderButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        orderDonutButton = (ImageButton) findViewById(R.id.openDonutButton);
+        orderCoffeeButton = (ImageButton) findViewById(R.id.openCoffeeButton);
+        viewOrderButton = (ImageButton) findViewById(R.id.viewOrderButton);
+        storeOrderButton = (ImageButton) findViewById(R.id.storeOrderButton);
     }
 }
