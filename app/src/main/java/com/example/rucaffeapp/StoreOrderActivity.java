233b74ecc6566c5e8233b3f18f644301a3de5e08
dimitrivictor1217@gmail.com <dimitrivictor1217@gmail.com>
2021-04-22 16:03:37 -0400

    Added main screeen image buttons

--- app/src/main/java/com/example/rucaffeapp/StoreOrderActivity.java
+++ app/src/main/java/com/example/rucaffeapp/StoreOrderActivity.java
@@ -10,19 +10,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.List;
 
+
+/**
+ * This class handles the operations for the store order.
+ * @author Pamdank Ambadipudi
+ * @author Dimitri Victor
+ */
 public class StoreOrderActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     ArrayAdapter<String> list_adapter;
     ArrayList<String> store_orders;
     private ActionBar actionbar;
 
     private ListView storeOrdersListView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,20 +48,36 @@ public class StoreOrderActivity extends AppCompatActivity implements AdapterView
 
     }
 
-
+    /**
+     * This function is called when an order is clicked
+     * @param parent the AdapterView where the click happened.
+     * @param view the view within the AdapterView that was clicked (this will be a view provided by the adapter)
+     * @param position the position of the view in the adapter
+     * @param id row id of the item that was clicked
+     */
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         remove(position);
     }
-//need to create in string.xml
+
+    /**
+     * This function creates the string for each order
+     * @param specific_order_list the list of menuItems in the given order
+     * @param orderNum this is the specific order's number
+     * @return returns the string order representation
+     */
     private String createString(ArrayList<String> specific_order_list, int orderNum){
-        String result = getString(R.string.orderNum) + orderNum + " ";
+        String result = getString(R.string.orderNum) + orderNum + ": ";
         for(int i = 0; i< specific_order_list.size(); i++){
-            result += specific_order_list.get(i) + " ";
+            result += specific_order_list.get(i) + ", ";
         }
         return result;
     }
 
+    /**
+     * This function returns the ArrayList of strings to be displayed on the listView
+     * @return an ArrayList of strings
+     */
     private ArrayList<String> get_string_orders(){
 
         ArrayList<String> store_orders = new ArrayList<>();
@@ -66,11 +88,14 @@ public class StoreOrderActivity extends AppCompatActivity implements AdapterView
             int order_index = all_order_nums.get(i);
             ArrayList<String> specific_order_list = MainActivity.storeOrder.getOrder(i).getOrderList();
             store_orders.add(createString(specific_order_list, order_index));
-
         }
-        return  store_orders;
+        return store_orders;
     }
 
+    /**
+     * This function is called when the user clicks an order to see if they want to delete an order
+     * @param position is the indexed position of the selected item in the ListView
+     */
     public void remove(int position){
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
         alert.setMessage(R.string.deleteThisMessage).setTitle(R.string.confirmation);
@@ -92,10 +117,9 @@ public class StoreOrderActivity extends AppCompatActivity implements AdapterView
 
     }
 
-    public void updateListView(){
-
-    }
-
+    /**
+     * This function is called to show a toast when an order is removed
+     */
     public void showToast(){
         Toast.makeText(this, R.string.orderRemoved, Toast.LENGTH_LONG).show();
     }
